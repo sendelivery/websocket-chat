ae93c4fd35d5dd479829b153692e4f01496a0cf6
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-06-09 21:47:17 +0100

    Introduced DequeWalker class to prevent excessive memory use (#3)
    * Introduced DequeWalker class to prevent excessive memory use
    
    * Removed commented out method declarations

--- tests/client/test_custom_widgets.py
+++ tests/client/test_custom_widgets.py
@@ -1,7 +1,7 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 import pytest
 import urwid
-from client.lib.ui.custom_widgets import Chatlog, InputBox, InfoPanel
+from client.lib.ui.custom_widgets import Chatlog, InputBox, InfoPanel, DequeWalker
 
 
 class TestChatlog:
@@ -135,3 +135,132 @@ class TestInfoPanel:
         # When & Then
         with pytest.raises(urwid.ExitMainLoop):
             infopanel.disconnect.keypress((5, 5), "enter")
+
+
+class TestDequeWalker:
+    @pytest.fixture
+    def walker_with_wrap(self) -> DequeWalker:
+        return DequeWalker(wrap_around=True, maxlen=3)
+
+    @pytest.fixture
+    def walker_no_wrap(self) -> DequeWalker:
+        return DequeWalker(wrap_around=False, maxlen=3)
+
+    def test_append_beyond_maxlen_pops_from_left(self, walker_no_wrap):
+        # Given
+        testdata = ["1", "2", "3", "4"]
+
+        # When
+        for message in testdata:
+            t = urwid.Text(message)
+            walker_no_wrap.append(t)
+
+        # Then
+        assert len(walker_no_wrap.contents) == 3
+
+        for text_widget in walker_no_wrap.contents:
+            assert text_widget.text != testdata[0]
+
+    @patch("client.lib.ui.custom_widgets.deque_walker.u.ListWalker._modified")
+    def test_set_focus_updates_focus_and_calls_modified(
+        self, mock_modified_func, walker_no_wrap
+    ):
+        # Given
+        walker_no_wrap.append("widget0")
+        walker_no_wrap.append("widget1")
+        assert mock_modified_func.call_count == 0
+
+        # When
+        walker_no_wrap.set_focus(1)
+
+        # Then
+        assert walker_no_wrap.focus == 1
+        assert mock_modified_func.call_count == 1
+
+    def test_set_focus_updates_focus_throws_for_invalid_position(self, walker_no_wrap):
+        # Given
+        walker_no_wrap.append("widget0")
+
+        # When & Then
+        with pytest.raises(IndexError):
+            walker_no_wrap.set_focus(1)
+
+    def test_next_position_returns_expected_position(
+        self, walker_no_wrap, walker_with_wrap
+    ):
+        # Happy path, no wrap
+        # Given
+        walker_no_wrap.append("widget0")
+        walker_no_wrap.append("widget1")
+        walker_no_wrap.append("widget2")
+
+        # When
+        pos = walker_no_wrap.next_position(1)
+
+        # Then
+        assert pos == 2
+
+        # Happy path, with wrap
+        # Given
+        walker_with_wrap.append("widget0")
+        walker_with_wrap.append("widget1")
+        walker_with_wrap.append("widget2")
+
+        # When
+        pos = walker_with_wrap.next_position(1)
+        wrapped_pos_edge = walker_with_wrap.next_position(3)
+        wrapped_pos_extraneous = walker_with_wrap.next_position(30)
+
+        # Then
+        assert pos == 2
+        assert wrapped_pos_edge == 0
+        assert wrapped_pos_extraneous == 0
+
+    def test_next_position_throws_for_invalid_position(self, walker_no_wrap):
+        # Given
+        walker_no_wrap.append("widget1")
+
+        # When & Then
+        with pytest.raises(IndexError):
+            walker_no_wrap.next_position(40)
+
+    def test_prev_position_returns_expected_position(
+        self, walker_no_wrap, walker_with_wrap
+    ):
+        # Happy path, no wrap
+        # Given
+        walker_no_wrap.append("widget0")
+        walker_no_wrap.append("widget1")
+        walker_no_wrap.append("widget2")
+
+        # When
+        pos = walker_no_wrap.prev_position(1)
+
+        # Then
+        assert pos == 0
+
+        # Happy path, with wrap
+        # Given
+        walker_with_wrap.append("widget0")
+        walker_with_wrap.append("widget1")
+        walker_with_wrap.append("widget2")
+
+        # When
+        pos = walker_with_wrap.prev_position(1)
+        wrapped_pos_left_edge = walker_with_wrap.prev_position(0)
+        wrapped_pos_right_edge = walker_with_wrap.prev_position(3)
+        wrapped_pos_extraneous = walker_with_wrap.prev_position(30)
+
+        # Then
+        assert pos == 0
+        assert wrapped_pos_left_edge == 2
+        assert wrapped_pos_right_edge == 2
+        assert wrapped_pos_extraneous == 29
+
+    def test_prev_position_throws_for_invalid_position(self, walker_no_wrap):
+        # Given
+        walker_no_wrap.append("widget1")
+
+        # When & Then
+        with pytest.raises(IndexError):
+            walker_no_wrap.prev_position(-40)
